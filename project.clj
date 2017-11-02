0536eeff25deb13263630251405e235089248871
David Granström <david@davidgranstrom.com>
2017-11-02 10:15:28 +0100

    Add LICENSE

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject wikipedia-extracts "0.1.0-SNAPSHOT"
   :description "Query wikipedia for article summaries"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GPLv3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.2.0"]
                  [org.clojure/data.json "0.2.6"]
